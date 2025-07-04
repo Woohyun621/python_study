6282ac85c495ae83efa2764670724f1e8538a76b
Woohyun621 <june13844@gmail.com>
2025-07-04 16:33:37 +0900

    250704 add txt

---
+++ tip.v
@@ -0,0 +1,8 @@
+cd "프로젝트를 다운 받을 파일의 이름"
+git clone "프로젝트 레파지토리 주소(복사)" "폴더 이름": 폴더 이름 안써주면 1의 폴더로 들어감
+cd "폴더이름"
+git add .
+-> git status (파일 상태 확인)
+git commit -m "커밋 이름"
+git pull origin master
+git push origin master
